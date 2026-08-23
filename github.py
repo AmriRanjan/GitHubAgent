3f6bf2fe2994dc754b76406d7e262542ba46004f
AmriRanjan <amriranjan07@gmail.com>
2026-08-23 15:43:36 +0100

    Initial commit for agent code

---
+++ github.py
@@ -0,0 +1,43 @@
+import os
+import requests
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+
+load_dotenv()
+
+github_token = os.getenv("GITHUB_TOKEN")
+
+def fetch_github(owner, repo, endpoint):
+    url = f"https://api.github.com/repos/{owner}/{repo}/{endpoint}"
+    headers = {
+        "Authorization": f"Bearer {github_token}"
+    }
+    response = requests.get(url, headers = headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("Failed with status code:", response.status_code)
+        return []
+
+def fetch_github_issues(owner, repo):
+    data = fetch_github(owner, repo, "issues")
+    return load_issues(data)
+
+def load_issues(issues):
+    docs = []
+    for entry in issues:
+        metadata = {
+            "author": entry["user"]["login"],
+            "comments": entry["comments"],
+            "body": entry["body"],
+            "labels": entry["labels"],
+            "created_at": entry["created_at"],
+        }
+        new_file = entry["title"]
+        if entry["body"]:
+            new_file += entry["body"]
+        doc = Document(page_content = new_file, metadata = metadata)
+        docs.append(doc)
+    return docs
